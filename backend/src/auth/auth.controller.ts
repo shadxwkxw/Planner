fdbca0084ba65d806da6f73634464958278fa7cf
shadxwkxw <lolkek2468@gmail.com>
2026-02-25 03:22:46 +0300

    Добавление logout, написана логика удаления refresh token из тела запроса и занос его в серверные cookie

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Res, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -9,14 +10,28 @@ export class AuthController {
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('login')
-  async login(@Body() dto: AuthDto) {
-    return this.authService.login(dto)
+  async login(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
+    const { refreshToken, ...response } = await this.authService.login(dto)
+    this.authService.addRefreshTokenToResponse(res, refreshToken)
+
+    return response
   }
 
   @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('register')
-  async register(@Body() dto: AuthDto) {
-    return this.authService.register(dto)
+  async register(@Body() dto: AuthDto, @Res({ passthrough: true }) res: Response) {
+    const { refreshToken, ...response } = await this.authService.register(dto)
+    this.authService.addRefreshTokenToResponse(res, refreshToken)
+
+    return response
+  }
+
+  @HttpCode(200)
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) res: Response) {
+    this.authService.removeRefreshTokenFromResponse(res)
+
+    return true
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,9 +3,13 @@ import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { AuthDto } from './dto/auth.dto';
 import { verify } from 'argon2';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
+  REFRESH_TOKEN_NAME = 'refreshToken'
+  EXPIRE_DAY_REFRESH_TOKEN = 1
+
   constructor(
     private jwt: JwtService,
     private userService: UserService
@@ -53,5 +57,28 @@ export class AuthService {
     if (!isValid) throw new UnauthorizedException('Invalid password')
 
     return user
+  }
+
+  addRefreshTokenToResponse(res: Response, refreshToken: string) {
+    const expiresIn = new Date() // указываем дату окончания. Берем текущую дату
+    expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN) // к дате окончания добавляем с помощью setDate() текущую дату и указываем количество дней, когда будет заканчиваться refresh token
+
+    res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, { // отдаем название cookie, что будет там лежать и важные опции
+      httpOnly: true, // указываем, что это серверный cookie
+      domain: 'localhost', // домен, к которому будет привязка. свой домен в prod
+      expires: expiresIn, // указываем дату окончания токена
+      secure: true, // опция показывает, что cookie будет https, иными словами - защищенная
+      sameSite: 'none' // lax in prod. Проверка на домены
+    })
+  }
+
+  removeRefreshTokenFromResponse(res: Response) {
+    res.cookie(this.REFRESH_TOKEN_NAME, '', {
+      httpOnly: true,
+      domain: 'localhost',
+      expires: new Date(0), // обнуление даты
+      secure: true,
+      sameSite: 'none'
+    })
   }
 }
