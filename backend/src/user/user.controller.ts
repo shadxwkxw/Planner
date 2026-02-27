24b86dc72928477378cfd48c318888f5eb4c1c37
shadxwkxw <lolkek2468@gmail.com>
2026-02-27 03:10:53 +0300

    Добавлен контроллер и сервис для получения и обновления профиля пользователя. Написан dto для пользователя и настроек pomodoro

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,25 @@
-import { Controller, Post, Body, UsePipes, HttpCode, ValidationPipe } from '@nestjs/common';
+import { Controller, Put, Body, UsePipes, HttpCode, ValidationPipe, Get } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { UserService } from './user.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { UserDto } from './user.dto';
 
-@Controller('user')
+@Controller('user/profile')
 export class UserController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  @Auth()
+  async profile(@CurrentUser('id') id:string) {
+    return this.userService.getProfile(id)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put()
+  @Auth()
+  async updateProfile(@CurrentUser('id') id:string, @Body() dto:UserDto) {
+    return this.userService.update(id, dto)
+  }
 }
